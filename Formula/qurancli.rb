7f1058ecffca85ac81bba2e0624288c905360cd2
Ahmed Saheed <saheed@quranCLI.com>
2022-06-30 17:08:42 +0100

    qurancli v1.0.0

--- Formula/qurancli.rb
+++ Formula/qurancli.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.0.0 at 2022-06-30T17:05:12.023759+01:00
+# Generated with JReleaser 1.0.0 at 2022-06-30T17:08:34.691416+01:00
 class Qurancli < Formula
   desc "A simple tool to Read, Search and Recite the Quran."
   homepage "https://github.com/ahmedsaheed/quranCLI"
